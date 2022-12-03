f526410fdd18b1f745771985e8e33e8ca9867865
Pedro Arantes <arantespp@gmail.com>
2022-12-03 17:56:04 -0300

    fix: export flashcard type (#15)

--- packages/zettel/src/index.ts
+++ packages/zettel/src/index.ts
@@ -1,3 +1,4 @@
 export { Zettelkasten } from './Zettelkasten';
 export type { ZettelkastenConfig } from './config';
 export type { Post, PostMetadata } from './files';
+export type { Flashcard } from './flashcard';
