2b8fd93399076ccb7d1c784ff0eea963268b293f
Pedro Arantes <arantespp@gmail.com>
2022-12-08 19:55:08 -0300

    feat: add 2d graph (#26)

--- packages/react-zettel/src/components/KnowledgeGraph.tsx
+++ packages/react-zettel/src/components/KnowledgeGraph.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
-import ForceGraph3D, { ForceGraphProps } from 'react-force-graph-3d';
+import ForceGraph2D, {
+  ForceGraphProps as ForceGraphProps2D,
+} from 'react-force-graph-2d';
+import ForceGraph3D, {
+  ForceGraphProps as ForceGraphProps3D,
+} from 'react-force-graph-3d';
 import SpriteText from 'three-spritetext';
 import type { GraphData } from '@tereza-tech/zettel/src/knowledgeGraph';
 
 export type { GraphData };
 
 export type KnowledgeGraphProps = {
-  height: number;
-  width: number;
+  height?: number;
+  width?: number;
   graphData: GraphData;
 };
 
@@ -16,10 +21,49 @@ export const KnowledgeGraph = ({
   width,
   graphData,
 }: KnowledgeGraphProps) => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+
+  const [dimensions, setDimensions] = React.useState({
+    height,
+    width,
+  });
+
+  /**
+   * Get dimensions of the parent element but only if the height and width
+   * props are not provided.
+   */
+  React.useEffect(() => {
+    const handleResize = () => {
+      if (containerRef.current) {
+        /**
+         * Get parent element dimensions
+         */
+        const parentDimensions =
+          containerRef.current.parentElement?.getBoundingClientRect();
+
+        /**
+         * Set dimensions state if the height and width props are not provided.
+         */
+        setDimensions({
+          height: height || parentDimensions?.height || 1000,
+          width: width || parentDimensions?.width || 1000,
+        });
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [width, height]);
+
   const newGraphData = React.useMemo(() => {
     const nodes = (graphData?.nodes || []).map((node) => {
       return {
-        val: node.group === 'notes' ? 10 : 1,
+        val: node.group === 'notes' ? 7 : 1,
         ...node,
       };
     });
@@ -33,9 +77,15 @@ export const KnowledgeGraph = ({
     return { nodes, links };
   }, [graphData?.links, graphData?.nodes]);
 
-  const graphCommonProps: ForceGraphProps = {
-    height,
-    width,
+  const [is3D, setIs3D] = React.useState(true);
+
+  const [dagMode, setDagMode] =
+    React.useState<ForceGraphProps2D['dagMode']>('radialin');
+
+  const graphCommonProps: ForceGraphProps3D & ForceGraphProps2D = {
+    ...dimensions,
+    dagMode,
+    dagLevelDistance: 50,
     graphData: newGraphData,
     nodeOpacity: 0.9,
     nodeResolution: 32,
@@ -43,21 +93,43 @@ export const KnowledgeGraph = ({
     linkWidth: 0.3,
     linkDirectionalParticles: 2,
     linkDirectionalParticleWidth: 3,
+
     // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
     onDagError: (loopNodeIds) => {},
   };
 
   return (
-    <ForceGraph3D
-      {...graphCommonProps}
-      dagMode="bu"
-      nodeThreeObjectExtend={true}
-      nodeThreeObject={(node: any) => {
-        const sprite = new SpriteText(node.label);
-        sprite.color = 'white';
-        sprite.textHeight = 4;
-        return sprite;
-      }}
-    />
+    <div ref={containerRef}>
+      {is3D ? (
+        <ForceGraph3D
+          {...graphCommonProps}
+          nodeThreeObjectExtend={true}
+          nodeThreeObject={(node: any) => {
+            const sprite = new SpriteText(node.label);
+            sprite.color = 'white';
+            sprite.textHeight = 4;
+            return sprite;
+          }}
+        />
+      ) : (
+        <ForceGraph2D {...graphCommonProps} />
+      )}
+      <div>
+        <button
+          onClick={() => {
+            setIs3D(!is3D);
+          }}
+        >
+          {is3D ? '2D' : '3D'}
+        </button>
+        <button
+          onClick={() => {
+            setDagMode(dagMode === 'bu' ? 'radialin' : 'bu');
+          }}
+        >
+          {dagMode === 'bu' ? 'Radial' : 'Bottom Up'}
+        </button>
+      </div>
+    </div>
   );
 };
