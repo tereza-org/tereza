5ab877a034e91e54980a6f75845fd983ea1fbea4
Pedro Arantes <arantespp@gmail.com>
2022-12-12 16:21:22 -0300

    fix: notes recommendations

--- apps/digital-garden/src/components/NotePage.tsx
+++ apps/digital-garden/src/components/NotePage.tsx
@@ -1,13 +1,19 @@
-import { Note } from '@tereza-tech/zettel';
+import { Note, NoteMetadata } from '@tereza-tech/zettel';
 import { Recommendations } from './Recommendations';
 
-export const NotePage = ({ note }: { note: Note }) => {
+export const NotePage = ({
+  note,
+  recommendations,
+}: {
+  note: Note;
+  recommendations: NoteMetadata[];
+}) => {
   return (
     <div>
       <h1>{note.title}</h1>
       {note.content}
       <hr />
-      <Recommendations recommendations={note.recommendations} />
+      <Recommendations recommendations={recommendations} />
     </div>
   );
 };
