7dcf7e0f34f8c7912ac5f3f4e41c0f29c4c81c2e
Pedro Arantes <arantespp@gmail.com>
2022-12-24 20:23:12 -0300

    fix: remove theme ui provider

--- apps/tereza/src/main.tsx
+++ apps/tereza/src/main.tsx
@@ -2,15 +2,14 @@ import { App } from './App';
 import { AuthProvider } from '@ttoss/react-auth';
 import { I18nProvider } from '@ttoss/react-i18n';
 import { NotificationsProvider } from '@ttoss/react-notifications';
-import { ThemeProvider } from '@ttoss/ui';
-import { theme } from '@tereza-tech/theme';
+import { ThemeProvider } from '@tereza-tech/theme';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './amplify';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider>
       <I18nProvider>
         <NotificationsProvider>
           <AuthProvider>
