1906c02aeb3c39e7f83a3fab1aadc37cf9c366fd
Pedro Arantes <arantespp@gmail.com>
2023-01-31 23:10:53 -0300

    feat: list notes (#41)

--- apps/tereza/src/main.tsx
+++ apps/tereza/src/main.tsx
@@ -2,6 +2,7 @@ import { App } from './App';
 import { AuthProvider } from '@ttoss/react-auth';
 import { I18nProvider } from '@ttoss/react-i18n';
 import { NotificationsProvider } from '@ttoss/react-notifications';
+import { RelayProvider } from './modules/ApiClient/RelayProvider';
 import { ThemeProvider } from '@tereza-tech/theme';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <I18nProvider>
         <NotificationsProvider>
           <AuthProvider>
-            <App />
+            <RelayProvider>
+              <App />
+            </RelayProvider>
           </AuthProvider>
         </NotificationsProvider>
       </I18nProvider>
--- apps/tereza/src/modules/Zettel/zettelRoutes.tsx
+++ apps/tereza/src/modules/Zettel/zettelRoutes.tsx
@@ -1,5 +1,6 @@
 import { RouteObject } from 'react-router-dom';
 import { ZettelForm, zettelFormAction } from './ZettelForm';
+import { ZettelHome, zettelHomeLoader } from './ZettelHome';
 import { ZettelRoot } from './ZettelRoot';
 
 export const zettelRoutes: RouteObject[] = [
@@ -7,6 +8,11 @@ export const zettelRoutes: RouteObject[] = [
     path: 'zettel',
     element: <ZettelRoot />,
     children: [
+      {
+        path: '',
+        loader: zettelHomeLoader,
+        element: <ZettelHome />,
+      },
       {
         path: 'edit',
         action: zettelFormAction,
