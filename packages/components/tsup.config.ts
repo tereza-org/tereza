4b1248b017201858a1e7238871d1e340aa99c932
Pedro Arantes <arantespp@gmail.com>
2023-02-16 09:01:04 -0300

    fix: build editor

--- packages/components/tsup.config.ts
+++ packages/components/tsup.config.ts
@@ -1,3 +1,14 @@
 import { tsupConfig } from '@ttoss/config';
 
-export const tsup = tsupConfig();
+export const tsup = tsupConfig({
+  external: [
+    '@lexical/code',
+    '@lexical/link',
+    '@lexical/list',
+    '@lexical/markdown',
+    '@lexical/rich-text',
+    '@lexical/selection',
+    '@lexical/table',
+    '@lexical/utils',
+  ],
+});
