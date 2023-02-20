b4296daab44b601d9b59a2da9b48d32eddf3a67f
Pedro Arantes <arantespp@gmail.com>
2023-02-20 19:14:38 -0300

    fix: remove color mode hook test

--- apps/tereza/src/App.tsx
+++ apps/tereza/src/App.tsx
@@ -1,16 +1,8 @@
-import * as React from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './routes';
-import { useTheme } from '@ttoss/ui';
 import './amplify';
 import '@tereza-tech/components/dist/index.css';
 
 export const App = () => {
-  const { setColorMode } = useTheme();
-
-  React.useEffect(() => {
-    setColorMode?.('dark');
-  }, []);
-
   return <RouterProvider router={createBrowserRouter(routes)} />;
 };
