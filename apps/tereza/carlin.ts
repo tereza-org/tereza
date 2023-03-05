4c60b01203dcae9fb77f3a6366d00714de75443f
Pedro Arantes <arantespp@gmail.com>
2023-03-05 14:49:47 -0300

    feat: add staging environment

--- apps/tereza/carlin.ts
+++ apps/tereza/carlin.ts
@@ -1,11 +1,15 @@
 export default {
+  acm: process.env.ACM_TEREZA_APP,
+  spa: true,
+  cloudfront: false,
+  hostedZoneName: 'tereza.app',
   environments: {
+    Staging: {
+      aliases: ['staging.tereza.app'],
+    },
     Production: {
-      acm: process.env.ACM_TEREZA_APP,
       aliases: ['tereza.app'],
       cloudfront: true,
-      hostedZoneName: 'tereza.app',
-      spa: true,
     },
   },
 };
